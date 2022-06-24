bcfd7e04ace1a88a65ebe576de7cb0c698cb724a
amanda <amandareisdw@gmail.com>
2022-06-23 22:15:46 -0300

    create history

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
         try {
             if(isSignup) {
                 const {login} = await signUp(data)
-                window.location.reload();
+                history.push('/login')
             } else {
                 const newData = {email: data.email, password: data.password}
                 const {login} = await signIn(newData)
